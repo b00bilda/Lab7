234dd1de4264f7ff182e30ecbd35bc31798faaae
Polina <yenina2000@inbox.ru>
2025-04-14 14:56:50 +0300

    2

--- server/src/main/java/org/example/managers/ServerEnvironment.java
+++ server/src/main/java/org/example/managers/ServerEnvironment.java
@@ -7,12 +7,7 @@ public class ServerEnvironment {
 
     CollectionManager collectionManager;
     CommandManager commandManager;
-    //DragonGenerator dragonGenerator;
     FileManager fileManager;
-    // Console console;
-    Scanner scanner;
-
-    String path = System.getenv("MY_FILE_PATH");
 
     public static ServerEnvironment getInstance() {
         if (environment == null) {
@@ -31,25 +26,10 @@ public class ServerEnvironment {
         return commandManager;
     }
 
-//    public DragonGenerator getDragonGenerator() {
-//        return dragonGenerator;
-//    }
-
     public FileManager getFileManager() {
         return fileManager;
     }
 
-//    public Console getConsole() {
-//        return console;
-//    }
-
-    public Scanner getScanner() {
-        return scanner;
-    }
-
-    public String getPath() {
-        return path;
-    }
 
     public void setCollectionManager(CollectionManager collectionManager) {
         this.collectionManager = collectionManager;
@@ -59,19 +39,7 @@ public class ServerEnvironment {
         this.commandManager = commandManager;
     }
 
-//    public void setDragonGenerator(DragonGenerator dragonGenerator) {
-//        this.dragonGenerator = dragonGenerator;
-//    }
-
     public void setFileManager(FileManager fileManager) {
         this.fileManager = fileManager;
-    }
-
-//    public void setConsole(Console console) {
-//        this.console = console;
-//    }
-
-    public void setScanner(Scanner scanner) {
-        this.scanner = scanner;
     }
 }
